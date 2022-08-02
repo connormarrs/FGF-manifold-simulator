0cde5406dcc374237b76f823683872fb0f713d25
connormarrs <cmarrs@bowdoin.edu>
2022-08-02 15:53:46 -0400

    Cleaned up errors in drivers.

--- python_scripts/s_0.5_expected_maxima.py
+++ python_scripts/s_0.5_expected_maxima.py
@@ -13,13 +13,13 @@ os.environ["OMP_NUM_THREADS"] = str(mp.cpu_count())
 # set parameters for the simulation
 dirichlet = True
 compute = True
-n_start = 5
-n_stop = 1000
-n_step = 5
+n_start = 1000
+n_stop = 20000
+n_step = 50
 
 s = 0.5
 
-numTrials = 10
+numTrials = 4000
 
 linspace = np.arange(start = n_start, stop = n_stop+n_step, step = n_step)
 sample = None
@@ -31,7 +31,7 @@ maxima = np.empty((linspace.shape[0], 2))
 
 for n_index in range(linspace.shape[0]):
 	if n_index == 0:
-		with open('../output/expected_maxima_s_'+str(s)+'_n_'+str(n_start)+'-'+str(n_stop)+'_numTrials_'+str(numTrials)+'.csv', 'w', newline = '') as csvFile:
+		with open('output/expected_maxima_s_'+str(s)+'_n_'+str(n_start)+'-'+str(n_stop)+'_numTrials_'+str(numTrials)+'.csv', 'w', newline = '') as csvFile:
 			writer = csv.writer(csvFile)
 			dfgf = DFGF_S1.DFGF_S1(s,linspace[n_index],numTrials,dirichlet,compute)
 			dfgf.runTrials()
@@ -41,7 +41,7 @@ for n_index in range(linspace.shape[0]):
 			row = [linspace[n_index], dfgf.getMeanOfMaxima()]
 			writer.writerow(row)
 	elif n_index>0:
-		with open('../output/expected_maxima_s_'+str(s)+'_n_'+str(n_start)+'-'+str(n_stop)+'_numTrials_'+str(numTrials)+'.csv', 'a', newline = '') as csvFile:
+		with open('output/expected_maxima_s_'+str(s)+'_n_'+str(n_start)+'-'+str(n_stop)+'_numTrials_'+str(numTrials)+'.csv', 'a', newline = '') as csvFile:
 			writer = csv.writer(csvFile)
 			dfgf = DFGF_S1.DFGF_S1(s,linspace[n_index],numTrials,dirichlet,compute)
 			dfgf.runTrials()
